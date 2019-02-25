3ba24714b02f032a630ecfad1c63981bb6b0f0ab
kejl <kejunliang@126.com>
2019-02-25 23:59:32 +0800

    增加路由

--- javascript/static/js/index.js
+++ javascript/static/js/index.js
@@ -1,1 +1,9 @@
-var temp;
+//用对象收编函数
+var checkobject={
+    checkname:function name() {
+        console.log("验证名称")
+    },
+    checkEmail:function name() {
+        console.log("验证邮箱")
+    }
+}
