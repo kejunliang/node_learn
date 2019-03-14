327f4bb466fde155270d90f01adcb5263637e397
kejl <kejunliang@126.com>
2019-03-14 19:32:23 +0800

    更新js文件

---
+++ hexo/source/_posts/js-this.md
@@ -0,0 +1,10 @@
+---
+title: js_this
+date: 2019-03-14 09:52:13
+tags:
+     - js
+categories:
+     - js
+---
+# 关于js中this指向的问题 
+### [转载Cnodejs](https://cnodejs.org/topic/5c813fd490c14711cc8cb5ae)
--- javascript/static/js/index.js
+++ javascript/static/js/index.js
@@ -90,8 +90,3 @@ function makeSizer(size) {
 
 
 
-window.onload=function(){
-    document.getElementById('size-12').onclick =  size12;
-document.getElementById('size-14').onclick =  size14;
-document.getElementById('size-16').onclick =  size16;
-}
