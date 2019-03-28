b9e0db34f61b33a881d7140c9386b920d26ab07e
kejl <kejunliang@126.com>
2019-03-28 11:10:48 +0800

    去掉angular旧项目新建新项目

--- angular/myoa/README.md
+++ angular/myoa/README.md
@@ -1,6 +1,6 @@
-# Javascripthome
+# Myoa
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.7.
 
 ## Development server
 
--- angular/myoa/src/app/app.component.spec.ts
+++ angular/myoa/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'javascripthome'`, () => {
+  it(`should have as title 'myoa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('javascripthome');
+    expect(app.title).toEqual('myoa');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to javascripthome!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to myoa!');
   });
 });
