cbb0146cc7c15d9100b645989277521e111251e2
kejl <kejunliang@126.com>
2019-04-10 14:05:21 +0800

    还原环境变量

--- my-project/src/environments/environment.ts
+++ my-project/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: `http://localhost:3000/`,
+  SERVER_URL: `./`,
   production: false,
   useHash: true,
   hmr: false,
