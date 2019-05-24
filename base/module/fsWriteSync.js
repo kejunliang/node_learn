8e997758d115d2bad03294d8eb062698e363804e
kejl <kejunliang@126.com>
2019-05-24 12:17:38 +0800

    增加问卷设计器静态页面

--- base/module/fsWriteSync.js
+++ base/module/fsWriteSync.js
@@ -2,3 +2,11 @@ var fs = require('fs');
 
 var data = 'Hello, Node.js 我来异步写数据了111111111';
 fs.writeFileSync('outputSync.txt', data);
+
+
+
+
+for(i=0;i<25;i++){
+    fs.writeFileSync('F:\\bigatt\\text'+i+'.txt',data+"="+i)
+    
+}
