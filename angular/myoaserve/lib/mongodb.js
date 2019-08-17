cc2d8a609dffd4bfa6c8496b1ac176d839c366ad
kejl <kejunliang@126.com>
2019-08-17 09:31:55 +0800

    增加mongodb依赖包

--- angular/myoaserve/lib/mongodb.js
+++ angular/myoaserve/lib/mongodb.js
@@ -1,9 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017";
  
-
-
-
 MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
     console.log('数据库已创建');
