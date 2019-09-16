0f66a83aef668970759c86dcc8aceceb621d7855
kejl <kejunliang@126.com>
2019-09-17 07:55:57 +0800

    add ionic

---
+++ ionic/myApp/src/app/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  //log in system
+  Login() {
+   alert("登录")
+  }
+
+
+}
