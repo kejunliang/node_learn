154ee3935a0c72ab9a23fc1e78aaf13ae11053b1
kejl <kejunliang@126.com>
2020-05-27 09:26:28 +0800

    vue路由学习和增加angularjs培训样例

---
+++ vue/antd-demo/src/components/dyncroutes-profile.vue
@@ -0,0 +1,17 @@
+<template>
+<div> 
+    <div>我是profile</div>
+    
+</div>
+ 
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: "kejl",
+    };
+  },
+   
+};
+</script>
